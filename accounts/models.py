80e3a5f41a140aacec05837b4261ae9024044af2
Alexandre <a-palo@laposte.net>
2015-12-19 21:47:51 +0100

    ajout des dossiers / fichiers maj de la class users
    Test Plan: no test plan
    
    Reviewers: pastys
    
    Reviewed By: pastys
    
    Differential Revision: http://phabricator.iresam.org/D74

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    surname = models.CharField(max_length=255)
-    family = models.CharField(max_length=255)
-    campus = models.CharField(max_length=4)
-    year = models.IntegerField()
+    surname = models.CharField(max_length=255, blank=True)
+    family = models.CharField(max_length=255, blank=True)
+    campus = models.CharField(max_length=4, blank=True)
+    year = models.IntegerField(default=0)
+    balance = models.FloatField(default=0)
