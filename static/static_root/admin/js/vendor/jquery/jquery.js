6d81740a1671124ac958a545172316be53e52ab1
Alexandre <a-palo@laposte.net>
2016-02-08 18:59:40 +0100

    JSON intial data
    Summary: Initial data sur shops
    
    Test Plan:
    migrate la bdd
    manage.py loaddata initial.json
    Vérifier en affichant les listes
    
    Reviewers: elek
    
    Differential Revision: http://phabricator.iresam.org/D119

--- static/static_root/admin/js/vendor/jquery/jquery.js
+++ static/static_root/admin/js/vendor/jquery/jquery.js
@@ -7843,7 +7843,7 @@ jQuery.extend({
 			text: "text/plain",
 			html: "text/html",
 			xml: "application/xml, text/xml",
-			json: "application/json, text/javascript"
+			json: "application/fixtures, text/javascript"
 		},
 
 		contents: {
@@ -7868,7 +7868,7 @@ jQuery.extend({
 			// Text to html (true = no transformation)
 			"text html": true,
 
-			// Evaluate text as a json expression
+			// Evaluate text as a fixtures expression
 			"text json": jQuery.parseJSON,
 
 			// Parse text as xml
@@ -8723,7 +8723,7 @@ jQuery.ajaxSetup({
 });
 
 // Detect, normalize options and install callbacks for jsonp requests
-jQuery.ajaxPrefilter( "json jsonp", function( s, originalSettings, jqXHR ) {
+jQuery.ajaxPrefilter( "fixtures jsonp", function( s, originalSettings, jqXHR ) {
 
 	var callbackName, overwritten, responseContainer,
 		jsonProp = s.jsonp !== false && ( rjsonp.test( s.url ) ?
@@ -8746,7 +8746,7 @@ jQuery.ajaxPrefilter( "json jsonp", function( s, originalSettings, jqXHR ) {
 			s.url += ( rquery.test( s.url ) ? "&" : "?" ) + s.jsonp + "=" + callbackName;
 		}
 
-		// Use data converter to retrieve json after script execution
+		// Use data converter to retrieve fixtures after script execution
 		s.converters["script json"] = function() {
 			if ( !responseContainer ) {
 				jQuery.error( callbackName + " was not called" );
@@ -8754,7 +8754,7 @@ jQuery.ajaxPrefilter( "json jsonp", function( s, originalSettings, jqXHR ) {
 			return responseContainer[ 0 ];
 		};
 
-		// force json dataType
+		// force fixtures dataType
 		s.dataTypes[ 0 ] = "json";
 
 		// Install callback
