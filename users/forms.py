1a613feb0113ee37d6ae0ef47ae93a591ff23f64
Alexandre <a-palo@laposte.net>
2016-02-14 21:05:25 +0100

    form user creation
    Summary: T67
    
    Test Plan: -
    
    Reviewers: elek, pastys
    
    Reviewed By: pastys
    
    Differential Revision: http://phabricator.iresam.org/D155

--- users/forms.py
+++ users/forms.py
@@ -9,8 +9,7 @@ from django.contrib.auth.models import Permission, Group
 class UserCreationCustomForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'first_name', 'last_name', 'surname', 'family', 'campus', 'year',
-                  'user_permissions', 'groups')
+        fields = ('username', 'first_name', 'last_name', 'surname', 'family', 'campus', 'year')
 
     def clean_username(self):
         username = self.cleaned_data['username']
@@ -25,15 +24,10 @@ class UserCreationCustomForm(UserCreationForm):
             code='duplicate_username',
         )
 
+    username = forms.CharField(label='Username', max_length=255)
     first_name = forms.CharField(label='Prenom', max_length=255)
     last_name = forms.CharField(label='Nom', max_length=255)
     surname = forms.CharField(label='Buque', max_length=255)
     family = forms.CharField(label='Fam\'ss', max_length=255)
     campus = forms.ChoiceField(label='Tabagn\'s', choices=User.CAMPUS_CHOICES)
     year = forms.ChoiceField(label='Prom\'ss', choices=User.YEAR_CHOICES)
-    user_permissions = forms.ModelMultipleChoiceField(label='Permissions', required=False,
-                                                      widget=forms.CheckboxSelectMultiple,
-                                                      queryset=Permission.objects.all())
-    groups = forms.ModelMultipleChoiceField(label='Groupes', required=False,
-                                            widget=forms.CheckboxSelectMultiple,
-                                            queryset=Group.objects.all())
