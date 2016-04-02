35e1086662a3d93b228048ecdc227e4bad389b56
pastys <a-palo@laposte.net>
2016-04-02 13:04:11 +0200

    ajout jquery ui dans static
    Summary: -
    
    Test Plan: -
    
    Reviewers: pastys
    
    Differential Revision: http://phabricator.iresam.org/D312

---
+++ static/static_root/js/widgets.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Alexandre on 31/03/2016.
+ */
+        jQuery(function($){
+            $('.datepicker').datepicker({
+                dateFormat : 'dd/mm/yy'
+            });
+        });
