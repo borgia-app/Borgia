72e7c5d7c0bcb4cb2a2ded0a3b96028e81fae545
Alexandre <a-palo@laposte.net>
2017-05-03 22:17:57 +0200

    wip in graphql shops

--- api/Schema/shops.py
+++ api/Schema/shops.py
@@ -1,15 +1,42 @@
 import graphene
-from graphene_django.types import DjangoObjectType
+from graphene_django.types import DjangoObjectType, ObjectType
 
-from shops.models import Shop
+from shops.models import (Shop, ContainerCase, ProductBase, ProductUnit,
+                          Container, SingleProduct)
 
 
 class ShopType(DjangoObjectType):
     class Meta:
         model = Shop
 
+
+class ContainerCase(DjangoObjectType):
+    class Meta:
+        model = ContainerCase
+
+class ProductBase(DjangoObjectType):
+    class Meta:
+        model = ProductBase
+
+class ProductUnit(DjangoObjectType):
+    class Meta:
+        model = ProductUnit
+
+class Container(DjangoObjectType):
+    class Meta:
+        model = Container
+
+class SingleProduct(DjangoObjectType):
+    class Meta:
+        model = SingleProduct
+
 class Query(graphene.AbstractType):
     all_shops = graphene.List(ShopType)
+    shop = graphene.Field(ShopType, id=graphene.ID(),
+                          description='The shop according to the ID')
 
     def resolve_all_shops(self, args, context, info):
         return Shop.objects.all()
+
+    def resolve_shop(self, args, context, info):
+        return Shop.objects.get(pk=args.get('id'))
