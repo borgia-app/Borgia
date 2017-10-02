1e4bbfe05bac0c7ce59e0b0836b9a6f15f78a9fe
Mael LACOUR <mael.lacour.web@gmail.com>
2017-10-02 10:39:02 +0200

    Fix: Add remark in db

--- finances/views.py
+++ finances/views.py
@@ -1399,7 +1399,7 @@ class SharedEventFinish(GroupPermissionMixin, FormView, GroupLateralMenuFormMixi
 
     def form_valid(self, form):
         self.se.done = True
-        # self.se.remark = form.cleaned_data['remark']
+        self.se.remark = form.cleaned_data['remark']
         self.se.save()
         if self.group.name == 'gadzarts':
             return redirect(
@@ -1507,7 +1507,7 @@ class SharedEventUpdate(GroupPermissionMixin, View, GroupLateralMenuMixin):
         context['order_by'] = 'last_name'
         context['done'] = se.done
         context['payment_error'] = payment_error
-        # context['remark'] = se.remark
+        context['remark'] = se.remark
         context['price'] = se.price
 
         return render(request, self.template_name, context=context)
@@ -1648,7 +1648,7 @@ class SharedEventUpdate(GroupPermissionMixin, View, GroupLateralMenuMixin):
         context['state'] = state
         context['order_by'] = order_by
         context['done'] = se.done
-        # context['remark'] = se.remark
+        context['remark'] = se.remark
         context['price'] = se.price
 
         return render(request, self.template_name, context=context)
