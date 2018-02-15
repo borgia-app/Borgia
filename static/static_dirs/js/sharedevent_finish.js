6778ef8bf2b05d70d57b4f85ff1d3ee51a8ef446
Mael LACOUR <mael.lacour.web@gmail.com>
2018-02-15 13:58:12 +0100

    Fix: SharedEvent : js for finish form

--- static/static_dirs/js/sharedevent_finish.js
+++ static/static_dirs/js/sharedevent_finish.js
@@ -1,33 +1,23 @@
-$(showTotalPrice()); // On laod
+$(updateForm()); // On laod
 
-$("#id_type_payment").change(function(){ // On change
+$("#id_type_payment").change(function(){
+         updateForm();
+    });
+
+function updateForm() {
   var selected = $("#id_type_payment option:selected").val();
   switch (selected) {
     case "pay_by_total":
-      showTotalPrice();
+      $( ".form-group" ).eq(1).show();
+      $( ".form-group" ).eq(2).hide();
       break;
     case "pay_by_ponderation":
-      showPonderationPrice();
+      $( ".form-group" ).eq(1).hide();
+      $( ".form-group" ).eq(2).show();
       break;
     case "no_payment":
-      hideBoth();
+      $( ".form-group" ).eq(1).hide();
+      $( ".form-group" ).eq(2).hide();
       break;
-    }
-});
-
-function showTotalPrice() {
-  // On load
-  $( ".form-group" ).eq(1).show();
-  $( ".form-group" ).eq(2).hide();
-}
-
-function showPonderationPrice() {
-  // On load
-  $( ".form-group" ).eq(1).hide();
-  $( ".form-group" ).eq(2).show();
-}
-
-function hideBoth() {
-  $( ".form-group" ).eq(1).hide();
-  $( ".form-group" ).eq(2).hide();
-}
+    };
+};
