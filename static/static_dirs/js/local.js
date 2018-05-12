fe4cc5a50e42b63fa20846e445072b10abfebd16
Wassim Belmezouar <wassimbel94@gmail.com>
2018-05-12 21:29:19 +0200

    Adapt Body padding-bottom to the footer

--- static/static_dirs/js/local.js
+++ static/static_dirs/js/local.js
@@ -2,6 +2,21 @@
    Our local JS file
 */
 
+// Adapt Body padding bottom to footer's height
+var adaptBodyPaddingToFooter = function() {
+	if ($(".footer").length > 0) {
+		height = $(".footer").height();
+		$('body').css("padding-bottom",height);
+	}
+}
+jQuery(function($){
+  adaptBodyPaddingToFooter();
+  
+  $(window).resize(function() {
+	  adaptBodyPaddingToFooter();
+  })
+})
+
 jQuery(function($){
   if ($(".notification .notification-actor").length > 0) {
      $(".notification .notification-actor").each(function() {
