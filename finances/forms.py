8fbff2a5e4e614b31c076ecdf6448387b345a6fa
Wassim Belmezouar <wassimbel94@gmail.com>
2018-06-18 00:12:52 +0200

    [Finances] SharedEventCreate: Add some JS form logic | SharedEventList: Pull right 'New' button

--- finances/forms.py
+++ finances/forms.py
@@ -265,10 +265,10 @@ class SharedEventCreateForm(forms.Form):
     price = forms.DecimalField(label='Prix total (vide si pas encore connu)', decimal_places=2, max_digits=9,
                                required=False, min_value=0)
     bills = forms.CharField(label='Factures liées (vide si pas encore connu)', required=False)
-    allow_self_registeration = forms.BooleanField(label='Autoriser la self préinscription', initial=True, required=False)
-    date_end_registration = forms.DateField(    label='Date de fin de self-préinscription (Si pas autorisé laisser vide)',
-                                                required=False,
-                                                widget=forms.DateInput(attrs={'class': 'datepicker'})
+    allow_self_registeration = forms.BooleanField(label='Autoriser la préinscription', initial=True, required=False)
+    date_end_registration = forms.DateField(label='Date de fin de la préinscription',
+                                            required=False,
+                                            widget=forms.DateInput(attrs={'class': 'datepicker'})
                                             )
 
 
