be21a9983e4ab459a4e7823c5cdb390bb7acdfea
Mael Lacour <mael.lacour.web@gmail.com>
2018-12-12 21:37:33 +0100

    [Configurations] Update tests

--- configurations/tests/tests_views.py
+++ configurations/tests/tests_views.py
@@ -9,44 +9,26 @@ class BaseConfigurationsViewsTest(BaseBorgiaViewsTestCase):
 
     def allowed_user_get(self):
         response_client1 = self.client1.get(
-            reverse(self.url_view, kwargs={'group_name': 'presidents'}))
+            reverse(self.url_view))
         self.assertEqual(response_client1.status_code, 200)
 
-    def not_existing_group_get(self):
-        response_client1 = self.client1.get(reverse(self.url_view,
-                                                    kwargs={'group_name': 'group_that_does_not_exist'}))
-        self.assertEqual(response_client1.status_code, 404)
-
-    def not_in_group_user_get(self):
-        response_client2 = self.client2.get(reverse(self.url_view,
-                                                    kwargs={'group_name': 'presidents'}))
-        self.assertEqual(response_client2.status_code, 403)
-
-    def not_allowed_group_get(self):
-        response_client2 = self.client2.get(reverse(self.url_view,
-                                                    kwargs={'group_name': 'externals'}))
+    def not_allowed_user_get(self):
+        response_client2 = self.client2.get(reverse(self.url_view))
         self.assertEqual(response_client2.status_code, 403)
 
     def offline_user_redirection(self):
-        response_offline_user = Client().get(
-            reverse(self.url_view, kwargs={'group_name': 'presidents'}))
+        response_offline_user = Client().get(reverse(self.url_view))
         self.assertEqual(response_offline_user.status_code, 302)
         self.assertRedirects(response_offline_user, '/auth/login/')
 
 class GlobalConfigTest(BaseConfigurationsViewsTest):
-    url_view = 'url_global_config'
+    url_view = 'url_list_config'
 
     def test_allowed_user_get(self):
         super().allowed_user_get()
 
-    def test_not_existing_group_get(self):
-        super().not_existing_group_get()
-
-    def test_not_in_group_user_get(self):
-        super().not_in_group_user_get()
-
-    def test_not_allowed_group_get(self):
-        super().not_allowed_group_get()
+    def test_not_allowed_user_get(self):
+        super().not_allowed_user_get()
 
     def test_offline_user_redirection(self):
         super().offline_user_redirection()
@@ -57,14 +39,8 @@ class CenterConfigTest(BaseConfigurationsViewsTest):
     def test_allowed_user_get(self):
         super().allowed_user_get()
 
-    def test_not_existing_group_get(self):
-        super().not_existing_group_get()
-
-    def test_not_in_group_user_get(self):
-        super().not_in_group_user_get()
-
-    def test_not_allowed_group_get(self):
-        super().not_allowed_group_get()
+    def test_not_allowed_user_get(self):
+        super().not_allowed_user_get()
 
     def test_offline_user_redirection(self):
         super().offline_user_redirection()
@@ -75,14 +51,8 @@ class PriceConfigTest(BaseConfigurationsViewsTest):
     def test_allowed_user_get(self):
         super().allowed_user_get()
 
-    def test_not_existing_group_get(self):
-        super().not_existing_group_get()
-
-    def test_not_in_group_user_get(self):
-        super().not_in_group_user_get()
-
-    def test_not_allowed_group_get(self):
-        super().not_allowed_group_get()
+    def test_not_allowed_user_get(self):
+        super().not_allowed_user_get()
 
     def test_offline_user_redirection(self):
         super().offline_user_redirection()
@@ -93,14 +63,8 @@ class LydiaConfigTest(BaseConfigurationsViewsTest):
     def test_allowed_user_get(self):
         super().allowed_user_get()
 
-    def test_not_existing_group_get(self):
-        super().not_existing_group_get()
-
-    def test_not_in_group_user_get(self):
-        super().not_in_group_user_get()
-
-    def test_not_allowed_group_get(self):
-        super().not_allowed_group_get()
+    def test_not_allowed_user_get(self):
+        super().not_allowed_user_get()
 
     def test_offline_user_redirection(self):
         super().offline_user_redirection()
@@ -111,14 +75,8 @@ class BalanceConfigTest(BaseConfigurationsViewsTest):
     def test_allowed_user_get(self):
         super().allowed_user_get()
 
-    def test_not_existing_group_get(self):
-        super().not_existing_group_get()
-
-    def test_not_in_group_user_get(self):
-        super().not_in_group_user_get()
-
-    def test_not_allowed_group_get(self):
-        super().not_allowed_group_get()
+    def test_not_allowed_user_get(self):
+        super().not_allowed_user_get()
 
     def test_offline_user_redirection(self):
         super().offline_user_redirection()
