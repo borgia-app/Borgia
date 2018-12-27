a9a626502c7143aeb1f67a366ee0dbffcb23a8d9
Mael Lacour <mael.lacour.web@gmail.com>
2018-12-28 00:19:18 +0100

    [Sales] Fix sale retrieve url. Fix module importation order in settings

--- borgia/settings.py
+++ borgia/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'users',
     'shops',
     'finances',
-    'sales',
     'events',
     'modules',
+    'sales',
     'stocks'
 ]
 
--- sales/urls.py
+++ sales/urls.py
@@ -7,6 +7,6 @@ from sales.views import SaleList, SaleRetrieve
 sales_patterns = [
         path('shops/<int:shop_pk>/sales/', include([
             path('', SaleList.as_view(), name='url_sale_list'),
-            path('<int:pk>/', SaleRetrieve.as_view(), name='url_sale_retrieve')
+            path('<int:sale_pk>/', SaleRetrieve.as_view(), name='url_sale_retrieve')
         ]))
 ]
