5ed5536a3482bc5ed0dfa6a7bc2307492ceafeb3
Alexandre PALO <a-palo@laposte.net>
2019-07-13 17:05:56 +0200

    anonymize manage.py

--- borgia/borgia/settings.py
+++ borgia/borgia/settings.py
@@ -8,23 +8,32 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
-import os
+'''
+⚠️⚠️⚠️
+THIS FILE IS FOR DEVELOPMENT ONLY.
+FOR PRODUCTION, USE THE FILE IN THE FOLDER PRODUCTION AND CHECK THE DOCUMENTATION.
+
+BEFORE USE:
+CHANGE PARAMETERS LABELLED AS "TO BE CHANGED" TO THE RIGHT VALUE.
+⚠️⚠️⚠️
+'''
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'need to be changed'
+SECRET_KEY = '⚠️TO BE CHANGED⚠️'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# 'www.borgia.iresam.org' en prod, '*' pour une simulation de prod en local.
 ALLOWED_HOSTS = ['*']
 
-# Application definition
 
+# Application definition
 INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -67,7 +76,6 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages'
             ],
-            # Permet de rajouter extra à la
             'builtins': ['users.templatetags.users_extra']
         },
     },
@@ -75,10 +83,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'borgia.wsgi.application'
 
-
 # Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -87,15 +92,13 @@ DATABASES = {
 }
 
 # Password validation
-# https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
-
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend'
 ]
 
 # Token auth backend
-TOKEN_CHECK_ACTIVE_USER = True
-TOKEN_TIMEOUT_DAYS = 7
+#TOKEN_CHECK_ACTIVE_USER = True
+#TOKEN_TIMEOUT_DAYS = 7
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -141,22 +144,26 @@ if DEBUG:
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 MEDIA_URL = '/media/'
 
-# Penser à activer 'autoriser l'acces par les applications moins sécurisées' dans Gmail
+
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'ae.ensam.assoc@gmail.com'
-SERVER_EMAIL = 'ae.ensam.assoc@gmail.com'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ae.ensam.assoc@gmail.com'
-EMAIL_HOST_PASSWORD = 'Alexandre57'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = '⚠️TO BE CHANGED⚠️'
+SERVER_EMAIL = '⚠️TO BE CHANGED⚠️'
+EMAIL_HOST = '⚠️TO BE CHANGED⚠️'
+EMAIL_PORT = '⚠️TO BE CHANGED⚠️'
+EMAIL_HOST_USER = '⚠️TO BE CHANGED⚠️'
+EMAIL_HOST_PASSWORD = '⚠️TO BE CHANGED⚠️'
+EMAIL_BACKEND = '⚠️TO BE CHANGED⚠️'
 
-ADMINS = []
 
-# Durée de validité du token reset password
+ADMINS = ['⚠️TO BE CHANGED⚠️']
+
+
+# Password reset validity duration
 PASSWORD_RESET_TIMEOUT_DAYS = 1  # en jours
 
-# Deconnection automatique
+
+# Automatic session logout
 SESSION_COOKIE_AGE = 7200
+
 
 DEFAULT_TEMPLATE = "light"  # Default template, en minuscule
