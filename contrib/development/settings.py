15cb0cdaa014e381bc4bcede46fbbf15b28a5157
Alexandre PALO <alexandrepalo@MacBook-Pro-de-Alexandre.local>
2021-05-06 21:45:08 +0200

    Update settings.py in contrib folders for Django 3 autofield

--- contrib/development/settings.py
+++ contrib/development/settings.py
@@ -24,6 +24,8 @@ BEFORE USE:
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Upgrading to Django 3
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'TO BE CHANGED'
