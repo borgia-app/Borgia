a25746d0f380e11c122324fde50292c4bf0d34c2
Alexandre PALO <a-palo@laposte.net>
2024-11-30 15:20:12 +0000

    install tailwindcss

--- borgia/static/static_root/js/finances/shared_event_create.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-  $("#id_allow_self_registeration").change(function() {
-    if (this.checked) {
-        $("#id_date_end_registration").prop('disabled', false);
-    } else {
-	    $("#id_date_end_registration").prop('disabled', true);
-	}
-  });
-});
